e7676d370d4336afc6a93422354343e5e28812bd
paridaabinash <abinashparida833@gmail.com>
2022-05-18 01:59:15 +0530

    completed first assignment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, { useState} from "react";
+import Functionbased from "./Functionbased";
+import Classbased from "./Classbased";
+
 import './App.css';
 
 function App() {
+  const [show, setShow] = useState({state:false});
+  const [showc, setShowc] = useState({state:false});
+
+  const functionBased = () => {
+    show.state ? setShow({state:false}) : setShow({state:true})
+  }
+
+  const classBased = () => {
+    showc.state ? setShowc({state:false}) : setShowc({state:true})
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="styling">
+        <h1>Styling Using Functional and Class Component</h1>
+        <button onClick={ () => functionBased() } >Div by using Functional Component</button>
+        <button onClick={ () => classBased() } >Div by using Class Component</button>
+        <section className="parentcomp">
+          {show.state && <Functionbased /> }
+          {showc.state && <Classbased />}
+        </section>
+      </div>
   );
 }
+
 
 export default App;
