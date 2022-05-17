81dab1807aca39bbbf2a7f0ecdcf0e39e59d9127
paridaabinash <99640788+paridaabinash@users.noreply.github.com>
2022-05-18 02:36:26 +0530

    Update Classbased.js

--- src/Classbased.js
+++ src/Classbased.js
@@ -1,13 +1,15 @@
 import React from 'react'
 
-const Classbased = () => {
-  return (
-    <div className="component classcard">
-    <h1>This is created using Class Components </h1>
-    <p>This is done using external CSS</p>
-    <p style={{color : 'blue'}}>This is done using inline CSS</p>
-  </div>    
-  )
+class Classbased extends React.Component {
+  render() {
+    return (
+      <div className="component classcard">
+        <h1>This is created using Class Components </h1>
+        <p>This is done using external CSS</p>
+        <p style={{color : 'blue'}}>This is done using inline CSS</p>
+      </div>    
+    )
+  }
 }
 
 export default Classbased;
